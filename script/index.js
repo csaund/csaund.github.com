0ccfd2706d70512a289e2b8c49c6530c96d662c1
Carolyn Saund <ca.m.saund@gmail.com>
2014-11-25 00:01:41 -0500

    tabs work added some text and comments

--- script/index.js
+++ script/index.js
@@ -2,14 +2,18 @@ $(document).ready(function(){
 	$(".my").addClass("hidden");
 	$(".who").removeClass("hidden");
 
+
+	//trolololo I'm so sorry I should
+	//have overwritten the default instead
+	//of made <a> elements but I was deciding
+	//whether to make a bunch of html files
+	//or do show/hide. this was so much easier.
 	$("a").on("click",function(){
 		$(".my").addClass("hidden");
 		var cl = $(this).attr('class');
 		cl = '.' + cl;
 		$(cl).removeClass("hidden");
-			
 	});
-
 });
 
 
